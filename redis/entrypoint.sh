505aa80354fe7ac7e526b326cf59dd6e924a18ac
ta1kin <ilataraskin475@gmail.com>
2025-10-03 09:54:41 +0300

    fix Dockerfile api

--- redis/entrypoint.sh
+++ redis/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "requirepass $REDIS_PASSWORD" > /usr/local/etc/redis.conf
+# Перезапишем пароль в конфиге
+sed -i "s|\${REDIS_PASSWORD}|$REDIS_PASSWORD|" /usr/local/etc/redis.conf
 exec redis-server /usr/local/etc/redis.conf
